2ca447aa7a8c77efeebee23afd07a210b2a746c6
Abdul Wahid <abdulwahid00771@gmail.com>
2026-04-21 18:42:38 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const app = ()=>{
+  return(
+    <>
+    <div id="dad">
+      <div id="son">I'm Son</div>
+      <div id="daughter">I'm Daughter</div>
+    </div>
+    <div id="uncle">
+      <div id="nephew">I'm Nephew</div>
+      <div id="niece">I'm Niece</div>
+    </div>
+    </>
+  )
+}
+
+export default app;
