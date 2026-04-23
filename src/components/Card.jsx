2526bf824e64fbaaa4f566ef797c521c2d81bfc4
Abdul Wahid <abdulwahid00771@gmail.com>
2026-04-23 18:03:00 +0500

    Card Project Completed!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import { Bookmark } from "lucide-react";
-const Card = () => {
+const Card = (props) => {
     return (
         <div id="card">
             <div className="card-top">
                 <div className="top-left">
                     <div className="org-icon">
-                        <img src="https://i.pinimg.com/736x/29/f6/41/29f641b507ccb60bb7a62a830b988736.jpg" alt="Organization Icon" />
+                        <img src={props.logo} alt="Organization Icon" />
                     </div>
                 </div>
                 <div className="top-right">
@@ -17,21 +17,20 @@ const Card = () => {
             </div>
             <div className="card-middle">
                 <div className="org-info">
-                    <h3 className="org-title">Amazon</h3>
-                    <span className="time-elapse">5 days ago</span>
+                    <h3 className="org-title">{props.company} <span className="time-elapse">{props.posted}</span></h3>
+                    
                 </div>
-                <h2 className="job-title">Senior UI/UX Designer</h2>
+                <h2 className="job-title">{props.title}</h2>
                 <div className="skills-container">
-                    <span className="skill">Part-Time</span>
-                    <span className="skill">Senior Level</span>
-                    <span className="skill">Senior Level</span>
-                    <span className="skill">Senior Level</span>
+                    {props.level.map((level)=>{
+                        return <span className="skill">{level}</span>
+                    })}
                 </div>
             </div>
             <div className="card-bottom">
                 <div className="bottom-left">
-                    <h3 className="salary">$120/hr</h3>
-                    <span className="job-location">Mumbai, India</span>
+                    <h3 className="salary">{props.salary}</h3>
+                    <span className="job-location">{props.location}</span>
                 </div>
                 <div className="bottom-right">
                     <a href="#" className="apply-btn">Apply Now</a>
